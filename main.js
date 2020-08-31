6cacf30a494d121b79ad5aea5fa9383b86423b25
functionadvanced <jupeizhong6@gmail.com>
2020-08-31 15:08:41 -0400

    update

--- main.js
+++ main.js
@@ -10,12 +10,22 @@ app.get('/', (req, res) => {
 
 app.get('/diagram', (req, res) => {
     res.sendFile(__dirname+'/diagram.html');
-}); 
-
-app.get('/diagram_interactive', (req, res) => {
-    res.sendFile(__dirname+'/diagram_interactive.html')
 });
 
 app.listen(port, () => {
     console.log(`listening at port:${port}`);
 });
+
+const {Builder, By, Key, until} = require('/usr/local/lib/node_modules/'+'selenium-webdriver');
+ 
+(async function example() {
+  let driver = await new Builder().forBrowser('chrome').build();
+  try {
+      await driver.get("localhost:"+port+"/diagram");
+    // await driver.get('http://www.google.com/ncr');
+    // await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
+    // await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+  } finally {
+    // await driver.quit();
+  }
+})();
